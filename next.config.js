4744be7f5496e5909c14ae61e5ed023bbf815ee4
Emerson Pedroso <emerson@bitcoinnano.org>
2024-08-29 05:04:44 -0300

    standalone

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  // Adicione a nova configuração aqui:
+  output: 'standalone' // Isso definirá o modo de saída do Next.js para 'standalone'
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
